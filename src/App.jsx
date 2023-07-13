e5303d34f876fbdb9f1cd168788264007457e35d
marcinhojazz <marcinhojazz@gmail.com>
2023-07-13 16:09:35 -0300

    update site create buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
 function App() {
+  const [activeTexture, setActiveTexture] = useState(0);
+
+  const handleButtonClick = (index) => {
+    setActiveTexture(index);
+  }
+
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience activeTexture={activeTexture} />
+      </Canvas>
+      <div id='group-buttons'>
+        <button onClick={() => handleButtonClick(0)}>Ambiente 1</button>
+        <button onClick={() => handleButtonClick(1)}>Ambiente 2</button>
+        <button onClick={() => handleButtonClick(2)}>Ambiente 3</button>
+      </div>
+    </>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,31 +1,22 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three"
 
-export const Experience = () => {
-
+export const Experience = ({ activeTexture }) => {
   const map = useTexture([
     "textures/anime_water_world.jpg",
     "textures/anime_world_of_fire_and_lava.jpg",
     "textures/anime_magic_forest_stars.jpg"
   ]);
-  
+
   return (
     <>
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
-      <mesh position={[1.5, 0, 0]}>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial map={map[0]} />
-      </mesh>
-      <mesh position={[-1.5, 0, 0]}>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial map={map[1]} />
-      </mesh>
       <mesh position={[0, 0, 0]}>
-        <sphereGeometry args={[0.5, 32, 32]} />
-        <meshStandardMaterial map={map[2]} />
+        <sphereGeometry args={[5, 32, 32]} />
+        <meshStandardMaterial map={map[activeTexture]} side={THREE.BackSide} />
       </mesh>
-      
     </>
   );
 };
